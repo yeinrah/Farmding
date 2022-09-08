fbf3c6e368bc590f9fe3ce980970504f2a7ac575
kimjeongseo <wjdtj9656@gmail.com>
2022-09-08 10:54:38 +0900

    FEAT: searchbar ui layout

--- Frontend/farmding/src/App.tsx
+++ Frontend/farmding/src/App.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import SearchBar from "./Common/UI/SearchBar/SearchBar";
+import FundingRanking from "./Pages/Funding/FundingRanking/FundingRanking";
 import Main from "./Pages/Main";
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Main />}></Route>
+        <Route path="/a" element={<SearchBar />}></Route>
+        <Route path="/r" element={<FundingRanking />}></Route>
       </Routes>
     </div>
   );
---
+++ Frontend/farmding/src/Pages/Funding/FundingRanking/FundingRanking.tsx
@@ -0,0 +1,6 @@
+import { Box } from "@mui/material";
+import styles from "./FundingRanking.module.css";
+const FundingRanking = () => {
+  return <Box>haha</Box>;
+};
+export default FundingRanking;
