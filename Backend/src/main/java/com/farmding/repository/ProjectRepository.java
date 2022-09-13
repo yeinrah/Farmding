f7c764ac6b8d55e85297ae6794fa6f0b231cc832
kimyoungjin <khss4008@gmail.com>
2022-09-13 17:49:12 +0900

    FEAT: api

--- Backend/src/main/java/com/farmding/repository/ProjectRepository.java
+++ Backend/src/main/java/com/farmding/repository/ProjectRepository.java
@@ -3,10 +3,11 @@ package com.farmding.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.farmding.db.entity.Project;
 
-
-public interface ProjectRepository extends JpaRepository<Project, Integer> {
+@Repository
+public interface ProjectRepository extends JpaRepository<Project, String> {
 	List<Project> findAll();
 }
--- Backend/src/main/java/com/farmding/service/FundingService.java
+++ Backend/src/main/java/com/farmding/service/FundingService.java
@@ -2,6 +2,7 @@ package com.farmding.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,10 +11,13 @@ import com.farmding.repository.ProjectRepository;
 
 @Service
 public class FundingService {
+	@Autowired
 	private ProjectRepository projectRepository;
+	
 	@Transactional
 	public List<Project> ProjectList() throws Exception {
-		return projectRepository.findAll();
+		List<Project> list = projectRepository.findAll();
+		return list;
 	}
 	
 }
