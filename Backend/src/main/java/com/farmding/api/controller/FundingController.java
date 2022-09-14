f495173d5b166592345b93e4652e5ba068233f32
kimyoungjin <khss4008@gmail.com>
2022-09-14 11:36:13 +0900

    FEAT: REST API

--- Backend/src/main/java/com/farmding/api/controller/FundingController.java
+++ Backend/src/main/java/com/farmding/api/controller/FundingController.java
@@ -1,15 +1,18 @@
 package com.farmding.api.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.farmding.db.entity.Like;
 import com.farmding.db.entity.Project;
 import com.farmding.service.FundingService;
 
@@ -35,5 +38,25 @@ public class FundingController {
 	public ResponseEntity<?> projectList() throws Exception {
 		List<Project> projectData = fundingService.ProjectList();
 		return new ResponseEntity<List<Project>>(projectData,HttpStatus.OK);
+	}
+	
+	@GetMapping("/popular_fundings")
+	@ApiOperation(value = "프로젝트 객체 8개", notes = "프로젝트 객체 중 Funder 수 순으로 상위 8개만 보냅니다.")
+	@ApiResponses({ @ApiResponse(code = 200, message = "가져오기 성공"), @ApiResponse(code = 400, message = "400에러"),
+		@ApiResponse(code = 409, message = "409에러"), @ApiResponse(code = 500, message = "500에러") })
+	public ResponseEntity<?> projectListTopEight() throws Exception {
+		List<Project> projectData = fundingService.ProjectListTopEight();
+		return new ResponseEntity<List<Project>>(projectData,HttpStatus.OK);
+	}
+	
+	@GetMapping("/like/{userId}")
+	@ApiOperation(value = "like 데이터", notes = "like")
+	@ApiResponses({ @ApiResponse(code = 200, message = "가져오기 성공"), @ApiResponse(code = 400, message = "400에러"),
+		@ApiResponse(code = 409, message = "409에러"), @ApiResponse(code = 500, message = "500에러") })
+	public ResponseEntity<?> zzimFunding(@PathVariable int userId) throws Exception {
+		List<Like> likeData = fundingService.likeFunding(userId);
+		List<Project> list = fundingService.getUserLikeProject(likeData);
+
+		return new ResponseEntity<List<Project>>(list,HttpStatus.OK);
 	}
 }
--- Backend/src/main/java/com/farmding/service/FundingService.java
+++ Backend/src/main/java/com/farmding/service/FundingService.java
@@ -1,23 +1,65 @@
 package com.farmding.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.farmding.db.entity.Like;
 import com.farmding.db.entity.Project;
+import com.farmding.repository.LikeRepository;
 import com.farmding.repository.ProjectRepository;
 
 @Service
 public class FundingService {
 	@Autowired
 	private ProjectRepository projectRepository;
+	@Autowired
+	private LikeRepository likeRepository;
+	
+	
+	public FundingService(ProjectRepository projectRepository, LikeRepository likeRepository) {
+		super();
+		this.projectRepository = projectRepository;
+		this.likeRepository = likeRepository;
+	}
 	
 	@Transactional
 	public List<Project> ProjectList() throws Exception {
 		List<Project> list = projectRepository.findAll();
 		return list;
 	}
+	@Transactional
+	public List<Project> ProjectListTopEight() throws Exception {
+		List<Project> list = projectRepository.findAllBestEight();
+		return list;
+	}
+	
+	@Transactional
+	public List<Like> likeFunding(int userId) throws Exception {
+		List<Like> list = likeRepository.findAllByUserId(userId);
+		return list;
+	}
+
+	@Transactional
+	public List<Project> getUserLikeProject(List<Like> likeData) throws Exception {
+		
+		List<Integer> idData = new ArrayList<>();
+		for(int i=0; i<likeData.size();i++) {
+			idData.add(likeData.get(i).getProjectId());
+			System.out.println("getProjectId==> "+likeData.get(i).getProjectId());
+		}
+		
+		List<Project> list = new ArrayList<Project>();
+		for(int i=0; i<idData.size();i++) {
+			list.add(projectRepository.findOneByProjectId(idData.get(i)));
+		}
+		
+		return list;
+	}
+	
+	
 	
 }
