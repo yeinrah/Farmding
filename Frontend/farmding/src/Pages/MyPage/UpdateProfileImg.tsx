0b4811cdf76e7866d6a49f205b8be157aebcba03
kimjeongseo <wjdtj9656@gmail.com>
2022-09-19 10:06:57 +0900

    FEAT: image dummy

--- Frontend/farmding/src/Pages/MyPage/UpdateProfileImg.tsx
+++ Frontend/farmding/src/Pages/MyPage/UpdateProfileImg.tsx
@@ -1,6 +1,6 @@
 import { Box } from "@mui/material";
 import { modalStyle } from "../../Common/data/Style";
-
+import profileImages from "../../Assets/profile/profileImages";
 const UpdateProfileImg = () => {
   return (
     <>
@@ -8,11 +8,26 @@ const UpdateProfileImg = () => {
         sx={{
           ...modalStyle,
           width: 500,
-          height: 500,
+          height: 430,
           overflow: "auto",
           backgroundColor: "#F6F49D",
         }}
-      ></Box>
+      >
+        <Box
+          sx={{ display: "flex", justifyContent: "center", flexWrap: "wrap" }}
+        >
+          {profileImages.map((element, index) => (
+            <Box>
+              <img
+                src={profileImages[index]}
+                alt="images"
+                style={{ width: "90px", height: "80px", cursor: "pointer" }}
+              />
+            </Box>
+            // <div>ha!!!!!!!!!!!!!!1123</div>
+          ))}
+        </Box>
+      </Box>
     </>
   );
 };
