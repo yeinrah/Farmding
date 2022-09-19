3bfbff8e747ad053f5b3138bfed5411e814b8933
송상훈 <sangong3@naver.com>
2022-09-19 14:34:23 +0900

    FEAT: basic code erc721

--- truffle-config.js
+++ truffle-config.js
@@ -43,7 +43,7 @@ module.exports = {
     // e.g., 가나슈 환경 
      development: {
       host: "127.0.0.1",     // Localhost (default: none)
-      port: 7545,            // Standard Ethereum port (default: none)
+      port: 8545,            // Standard Ethereum port (default: none)
       network_id: "*",       // Any network (default: none)
      },
     // Another network with more advanced options...
