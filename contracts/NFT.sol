79fde889e3a8762ead8f252672f4388801dc76be
kimyoungjin <khss4008@gmail.com>
2022-09-20 10:31:44 +0900

    FEAT: openzepplin token file insert

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -15,8 +15,8 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "./token/ERC721/ERC721Enumerable.sol";
+import "./access/Ownable.sol";
 
 contract NFT is ERC721Enumerable, Ownable {
   using Strings for uint256;
