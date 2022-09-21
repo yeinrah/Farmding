496ce1eb85a3cd6e0556919d99106854eb20b567
송상훈 <sangong3@naver.com>
2022-09-21 15:01:15 +0900

    FEAT: deal - purchase

--- smart-contracts/migrations/1_initial_migration.js
+++ smart-contracts/migrations/1_initial_migration.js
@@ -1,13 +1,11 @@
 //const Migrations = artifacts.require("Migrations");
 const SsafyToken = artifacts.require("SsafyToken");
-const SsafyNFT = artifacts.require("SsafyNFT");
 const DealFactory = artifacts.require("DealFactory");
 const Funding = artifacts.require("Funding");
 const NFT = artifacts.require("NFT");
 
 
 module.exports = (deployer) =>{
-  deployer.deploy(SsafyNFT); 
   deployer.deploy(SsafyToken, "SSAFY", "SSF", 0);
   deployer.deploy(DealFactory);
   deployer.deploy(Funding);
