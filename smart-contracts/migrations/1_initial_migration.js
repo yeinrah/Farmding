95e858e707fdee10b557cde0b8223e496482bd7b
송상훈 <sangong3@naver.com>
2022-09-21 11:16:03 +0900

    FEAT: nft deal create

--- smart-contracts/migrations/1_initial_migration.js
+++ smart-contracts/migrations/1_initial_migration.js
@@ -1,22 +1,15 @@
 //const Migrations = artifacts.require("Migrations");
 const SsafyToken = artifacts.require("SsafyToken");
 const SsafyNFT = artifacts.require("SsafyNFT");
-const SaleFactory = artifacts.require("SaleFactory");
+const DealFactory = artifacts.require("DealFactory");
 const Funding = artifacts.require("Funding");
 const NFT = artifacts.require("NFT");
 
-/**
- * PJT Ⅰ/Ⅲ - 시나리오 테스트
- * @dev 
- * 올바른 테스트를 위해 
- * PJT Ⅰ - SsafyNFT
- * PJT Ⅲ - SsafyNFT, SsafyToken, SaleFactory
- * 가 배포되어야 합니다. 
- */
+
 module.exports = (deployer) =>{
   deployer.deploy(SsafyNFT); 
   deployer.deploy(SsafyToken, "SSAFY", "SSF", 0);
-  deployer.deploy(SaleFactory);
+  deployer.deploy(DealFactory);
   deployer.deploy(Funding);
   deployer.deploy(NFT);
 };
