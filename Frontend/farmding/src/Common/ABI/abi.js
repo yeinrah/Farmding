547b7f67b17a60a3650d6d1a63088833798f58b9
kimjeongseo <wjdtj9656@gmail.com>
2022-10-05 15:58:13 +0900

    STYLE:smart contract address changed

--- Frontend/farmding/src/Common/ABI/abi.js
+++ Frontend/farmding/src/Common/ABI/abi.js
@@ -1195,7 +1195,7 @@ const ssafyTokenAbi = [
     type: "function",
   },
 ];
-export const NFTAddress = "0xbC2067BE7689cBD589474b6a7c271d69c9e2F156";
+export const NFTAddress = "0x6E74A5ca1Bf274185a1E9F2756bb7118A8263357";
 export const SSFTokenAddress = "0x0c54E456CE9E4501D2c43C38796ce3F06846C966";
 
 export const web3 = new Web3(window.ethereum);
